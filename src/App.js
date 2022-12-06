5d034eb1fe8e5d74a3237aa9f759be5ea04fa468
nbmyng <nbmyng@gmail.com>
2022-12-06 11:56:06 +0900

    feat: login form & validation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      disabled: true
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleInputChange(e) {
+    const value = e.target.value;
+    const name = e.target.name;
+
+    this.setState({
+      [name]: value
+    });
+
+    (/@/.test(this.state.email)) && (this.state.password.length >= 8) ? this.setState({disabled: false}) : this.setState({disabled: true});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+  
+  render() {
+    const email = this.state.email
+    const password = this.state.password
+    const disabled = this.state.disabled
+
+    return(
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            E-mail
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={this.handleInputChange}>
+            </input>
+          </label>
+          <label>
+            Password
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={this.handleInputChange}>
+            </input>
+          </label>
+          <button type="submit" disabled={disabled}>
+            로그인
+          </button>
+        </form>
+        {this.state.password.length}
+      </div>
+    )
+  }
 }
 
-export default App;
+
+export default App
